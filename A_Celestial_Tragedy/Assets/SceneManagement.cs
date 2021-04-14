27fc39bde303b1be457821b75c0006b240b6a93e
Carlos Melendez <carlos.melendez.255.@my.csun.edu>
2021-04-13 22:44:05 -0700

    Improved bugs, camera changes
    - jiv helped fix errors
    -still contains errors regarding VR and sound spatial but easy to resolve

--- A_Celestial_Tragedy/Assets/SceneManagement.cs
+++ A_Celestial_Tragedy/Assets/SceneManagement.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public string nameOfScene; 
+ private void OnTriggerEnter(Collider other) {
+     if(other.gameObject.tag== "Player") {
+    SceneManager.LoadScene(nameOfScene);
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     }
+ }
 }
